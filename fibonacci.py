2f268cf809800003fc12aeff9559d6de6a0521ea
valex66 <tahn.viot12@gmail.com>
2024-09-19 19:27:24 +0200

    v2 : using argparse

--- fibonacci.py
+++ fibonacci.py
@@ -2,13 +2,17 @@
 la suite de fibonacci
 """
 
-cache={0:0,1:1}
+# on optimise
 
+from functools import cache
+
+@cache
 def fibo_cached(n):
-    if n in cache:
-        return cache[n]
-    if n<0:
-        return False
-    value= fibo_cached(n-2)+fibo_cached(n-1)
-    cache[n]=value
-    return value
+    """
+    en cachant les résultats on arrive
+    à une complexité linéaire
+    """
+    if n <= 1:
+        return n
+    else:
+        return fibo_cached(n-1) + fibo_cached(n-2)
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-# avec sys.argv on peut lire la ligne de commande
-import sys
+# avec argparse aussi on peut lire la ligne de commande
+# et c'est beaucoup plus puissant
+from argparse import ArgumentParser
 
 # on importe le module de calcul
 from fibonacci import fibo_cached
 
 # acquisition du paramètre à partir de la ligne de commande
-n = int(sys.argv[1])
+parser = ArgumentParser()
+parser.add_argument('n', type=int)
+args = parser.parse_args()
+n = args.n
 
 # calcul et affichage
-print(f"fibo({n}) = {fibo_cached(n)}")
+print(f"fibo_cached({n}) = {fibo_cached(n)}")
